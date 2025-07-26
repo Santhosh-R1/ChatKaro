3cd749824e589c724224eec869b616f37c24a055
Santhosh-R1 <santhoshrajan817@gmail.com>
2025-07-26 20:22:01 +0530

    tytytyty

--- Chat/src/main.jsx
+++ Chat/src/main.jsx
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import reducer, { initialState } from "./Componets/ContextApi/reducer";
 import { StateProvider } from "./Componets/ContextApi/StateProvider";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
+      <SpeedInsights />
     </StateProvider>
   </StrictMode>
 );
